9db87a188896cb6f379bbab109825ea52b52f28c
Vyacheslav Deych <plumsoftwareofficial@gmail.com>
2026-03-26 15:57:36 +0600

    Реклама для гугл плей

--- app/src/main/java/ru/plumsoftware/avocado/data/ads/AdsConfig.kt
+++ app/src/main/java/ru/plumsoftware/avocado/data/ads/AdsConfig.kt
@@ -4,7 +4,7 @@ import ru.plumsoftware.avocado.BuildConfig
 
 object AdsConfig {
     val APP_OPEN_ADS_ID: String =
-        if (BuildConfig.DEBUG) "demo-appopenad-yandex" else if (BuildConfig.PLATFORM == 1) "R-M-18957454-1" else if (BuildConfig.PLATFORM == 2) "R-M-18957685-1" else "demo-appopenad-yandex"
+        if (BuildConfig.DEBUG) "demo-appopenad-yandex" else if (BuildConfig.PLATFORM == 1) "R-M-18957454-1" else if (BuildConfig.PLATFORM == 2) "R-M-18957685-1" else if (BuildConfig.PLATFORM == 3) "R-M-19002616-2" else "demo-appopenad-yandex"
     val INTERSTITIAL_ADS_ID: String =
-        if (BuildConfig.DEBUG) "demo-interstitial-yandex" else if (BuildConfig.PLATFORM == 1) "R-M-18957454-2" else if (BuildConfig.PLATFORM == 2) "R-M-18957685-2" else "demo-interstitial-yandex"
+        if (BuildConfig.DEBUG) "demo-interstitial-yandex" else if (BuildConfig.PLATFORM == 1) "R-M-18957454-2" else if (BuildConfig.PLATFORM == 2) "R-M-18957685-2" else if (BuildConfig.PLATFORM == 3) "R-M-19002616-1" else "demo-interstitial-yandex"
 }
