097a93e7fe92ee5271ab9abf2948bc5958273515
Vyacheslav Deych <plumsoftwareofficial@gmail.com>
2026-03-31 12:33:09 +0600

    Поднял версию

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.plumsoftware.avocado"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
